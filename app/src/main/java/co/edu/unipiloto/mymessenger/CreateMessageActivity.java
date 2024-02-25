889cc0d3bc6717ab42a40660e744c3d47af31969
BuildTools <unconfigured@null.spigotmc.org>
2024-02-25 17:32:47 -0500

    Update

--- app/src/main/java/co/edu/unipiloto/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/co/edu/unipiloto/mymessenger/CreateMessageActivity.java
@@ -6,21 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "messageText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+        updateMessageHistory();
     }
 
     public void onSendMessage(View view) {
         EditText editText = (EditText)findViewById(R.id.message);
         String messageText = editText.getText().toString();
+        MessageStorage.messages.add("Propietario: " + messageText);
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
+        updateMessageHistory();
     }
 
+    private void updateMessageHistory() {
+        TextView messageHistoryView = (TextView)findViewById(R.id.messageHistory);
+        StringBuilder messagesText = new StringBuilder();
+        for(String message : MessageStorage.messages) {
+            messagesText.append(message).append("\n");
+        }
+        messageHistoryView.setText(messagesText.toString());
+    }
 }
--- app/src/main/java/co/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/co/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
@@ -3,20 +3,34 @@ package co.edu.unipiloto.mymessenger;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ReceiveMessageActivity extends Activity {
-
-    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        updateMessageHistory();
+    }
 
-        Intent intent = getIntent();
-        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-        TextView messageView = (TextView)findViewById(R.id.message);
-        messageView.setText(messageText);
+    public void onSendMessage(View view) {
+        EditText editText = (EditText)findViewById(R.id.message2);
+        String messageText = editText.getText().toString();
+        MessageStorage.messages.add("Conductor: " + messageText);
+        Intent intent = new Intent(this, CreateMessageActivity.class);
+        startActivity(intent);
+        updateMessageHistory();
+    }
+
+    private void updateMessageHistory() {
+        TextView messageHistoryView = (TextView)findViewById(R.id.messageHistory);
+        StringBuilder messagesText = new StringBuilder();
+        for(String message : MessageStorage.messages) {
+            messagesText.append(message).append("\n");
+        }
+        messageHistoryView.setText(messagesText.toString());
     }
 }
